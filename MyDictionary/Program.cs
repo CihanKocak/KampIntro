06fde3a9b9253182d8757b05db7df5dbcedbe0ca
CihanKocak <69902606+CihanKocak@users.noreply.github.com>
2021-01-22 15:31:15 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionaryList<string, string> sehirler = new MyDictionaryList<string, string>();
+            sehirler.Add("06", "Ankara");
+
+            //Console.WriteLine(sehirler);
+
+            sehirler.Add("07", "Antalya");
+
+            //Console.WriteLine(sehirler);
+
+            foreach (var plakalar in sehirler.Keys)
+            {
+                Console.WriteLine(plakalar);
+            }
+
+            foreach (var sehir in sehirler.Values)
+            {
+                Console.WriteLine(sehir);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
